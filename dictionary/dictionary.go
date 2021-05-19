c8aee524daa1e0b52de894cc7c6bc8fa5bd34d76
jon.lopez <jonathan.lopez@gridunity.com>
2021-05-19 16:38:29 -0400

    Adding badger dependency

---
+++ dictionary/dictionary.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	badger "github.com/dgraph-io/badger/v3"
+)
+
+type Entries struct {
+	Entries []Entry `json:"entries"`
+}
+
+type Entry struct {
+	Word       string `json:"word"`
+	Pos        string `json:"type"`
+	Definition string `json:"description"`
+}
+
+type Definition struct {
+	partOfSpeech string
+	definition   string
+}
+
+type Dictionary struct {
+	words map[string][]Definition
+}
+
+func NewDictionary() Dictionary {
+	return Dictionary{
+		words: make(map[string][]Definition),
+	}
+}
+
+func (dict *Dictionary) Insert(entry Entry) {
+	definition := Definition{
+		partOfSpeech: entry.Pos,
+		definition:   entry.Definition,
+	}
+	dict.words[entry.Word] = append(dict.words[entry.Word], definition)
+}
+
+func (dict *Dictionary) Count() int {
+	return len(dict.words)
+}
+
+func (dict *Dictionary) List() []string {
+	var wordList []string
+
+	for word := range dict.words {
+		wordList = append(wordList, word)
+	}
+	return wordList
+}
+
+func (dict *Dictionary) Read(word string) {
+	fmt.Println(word)
+	for _, def := range dict.words[word] {
+		fmt.Println(def.partOfSpeech, def.definition)
+	}
+}
+
+func readJSONDictionary(dictionaryFile string) Dictionary {
+	jsonFile, err := os.Open(dictionaryFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var entries Entries
+	json.Unmarshal(byteValue, &entries)
+
+	dict := NewDictionary()
+	for _, entry := range entries.Entries {
+		dict.Insert(entry)
+	}
+	return dict
+}
+
+func main() {
+	dict := readJSONDictionary("dictionary.json")
+	fmt.Println(dict.Count())
+	// fmt.Println(dict.List())
+	dict.Read("Canter")
+}
--- dictionary/go.mod
+++ dictionary/go.mod
@@ -1,3 +1,5 @@
 module godict/dictionary
 
 go 1.16
+
+require github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
