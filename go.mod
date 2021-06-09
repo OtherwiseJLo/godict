eed4ca6f6d8f2b4405ec932bd1a61c32332c274a
OtherwiseJLo <jonathanglopez@gmail.com>
2021-06-09 09:00:41 -0400

    Updates to tui

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module godict
 go 1.16
 
 require (
+	github.com/charmbracelet/bubbles v0.8.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.21.10 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"log"
 )
 
 type model struct {
-	searchBox string
+	searchBox textinput.Model
 	choices   []string
 	cursor    int
 	selected  map[int]struct{}
+	err       error
 }
 
-var initialModed = model{
-	searchBox: "",
-	choices:   []string{},
-	selected:  make(map[int]struct{}),
-}
+func initialModel() model {
+	search := textinput.NewModel()
+	search.Placeholder = ""
+	search.Focus()
+	search.CharLimit = 100
+	search.Width = 30
 
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-
+	return model{
+		searchBox: search,
+		choices:   []string{},
+		selected:  make(map[int]struct{}),
 	}
 }
 
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+	// We handle errors just like any other message
+	case tea.errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.searchBox, cmd = m.searchBox.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"What’s your favorite Pokémon?\n\n%s\n\n%s",
+		m.searchBox.View(),
+		"(esc to quit)",
+	) + "\n"
+}
 func main() {
-	p := tea.NewProgram()
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
